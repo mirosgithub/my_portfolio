bd9ce6f7c88a34962966253aa94a0009d04e3780
Suah Kim <skim460@aucklanduni.ac.nz>
2025-07-04 12:37:23 +1200

    feat: implement homepage and add about page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from config import PERSONAL_INFO
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', personal_info=PERSONAL_INFO)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', personal_info=PERSONAL_INFO)
