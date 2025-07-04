c8234edc34cda5575801a66e035ecc0dcb4f4994
Suah Kim <skim460@aucklanduni.ac.nz>
2025-07-04 12:53:21 +1200

    feat: add project data and implement projects page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from config import PERSONAL_INFO
+from config import PERSONAL_INFO, PROJECTS_DATA
 
 app = Flask(__name__)
 
@@ -10,3 +10,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', personal_info=PERSONAL_INFO)
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', projects=PROJECTS_DATA, personal_info=PERSONAL_INFO)
--- config.py
+++ config.py
@@ -8,3 +8,24 @@ PERSONAL_INFO = {
     'github': 'https://github.com/mirosgithub',
     'email': os.getenv('EMAIL')
 }
+
+PROJECTS_DATA = [
+    {
+        'title': 'Personal Portfolio Website', 
+        'description': 'description',
+        'github_url': None, 
+        'live_url': 'http://127.0.0.1:5000/'
+    }, 
+    {
+        'title': 'Project 2', 
+        'description': 'description',
+        'github_url': None, 
+        'live_url': None
+    },
+    {
+        'title': 'Project 3', 
+        'description': 'description',
+        'github_url': None, 
+        'live_url': None
+    }
+]
