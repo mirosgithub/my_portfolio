bffcb03b44cf3b47ebe3d0295e1e0560dbcaa23b
Suah Kim <skim460@aucklanduni.ac.nz>
2025-07-04 17:19:50 +1200

    feat: add email notification system for contact form submissions

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from flask import Flask, render_template, flash, redirect, url_for
-from config import PERSONAL_INFO, PROJECTS_DATA, SECRET_KEY
+from config import PERSONAL_INFO, PROJECTS_DATA, SECRET_KEY, SMTP_CONFIG
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, EmailField
 from wtforms.validators import InputRequired
@@ -12,6 +15,33 @@ class ContactForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired()])
     message = TextAreaField('Message', validators=[InputRequired()])
     submit = SubmitField('Send')
+    
+def send_notification_email(name, email, message):
+    smtp_email = SMTP_CONFIG['EMAIL']
+    smtp_server = SMTP_CONFIG['SERVER']
+    smtp_port = SMTP_CONFIG['PORT']
+    smtp_password = SMTP_CONFIG['PASSWORD']
+    
+    subject = 'Contact Form Update'
+    body = f'From: {name}({email})\nMessage: {message}'
+    
+    try:
+        message = MIMEMultipart()
+        message['From'] = smtp_email
+        message['To'] = smtp_email
+        message['Subject'] = subject
+        
+        message.attach(MIMEText(body))
+        
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_email, smtp_password)
+            server.sendmail(smtp_email, smtp_email, message.as_string())
+        
+        print('Notification sent')
+            
+    except Exception as e:
+        print(f'Error sending notification: {e}')
 
 @app.route('/')
 def home():
@@ -28,8 +58,15 @@ def projects():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
+    
     if form.validate_on_submit():
-        first_name = form.name.data.split()[0]
-        flash(f"Thanks for reaching out, {first_name}! I'll be in touch with you soon.")
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        
+        send_notification_email(name, email, message)
+        flash(f"Thanks for reaching out, {name.split()[0]}! I'll be in touch with you soon.")
+
         return redirect(url_for('contact'))
+    
     return render_template('contact.html', title='Contact', form=form)
--- config.py
+++ config.py
@@ -31,3 +31,10 @@ PROJECTS_DATA = [
 ]
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+
+SMTP_CONFIG = {
+    'SERVER' : "smtp.gmail.com",
+    'PORT' : 587,
+    'EMAIL' : os.getenv('EMAIL'),
+    'PASSWORD' : os.getenv('EMAIL_PASSWORD')
+}
