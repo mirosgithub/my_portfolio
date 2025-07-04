602a9e29728b8ed1efa39b917eda92d75bb8ddec
Suah Kim <skim460@aucklanduni.ac.nz>
2025-07-04 12:37:21 +1200

    feat: setup config file and environment support

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+PERSONAL_INFO = {
+    'name': 'Suah Kim',
+    'title': 'Software Engineering Student',
+    'bio': "Hi! my name is Suah, and I'm a pre-penultimate software engineering student at the University of Auckland. I'm interested in full stack development, machine learning, and algorithmic trading, with experience in Python, Java, and basic web technologies. I thrive on new challenges and am always eager to learn and apply my technical skills to real-world problems.",
+    'profile_image': 'profile.jpg', 
+    'github': 'https://github.com/mirosgithub',
+    'email': os.getenv('EMAIL')
+}
